db1f8f7f0a61308d4bf096164fe7fde3589346be
ndegwa Wa Ndungu <ndegwaduncan193@gmail.com>
2022-10-15 06:41:24 +0000

    implemented creation of and saving of new reply to a thread with the subsequent return of the thread id, thread text and the list of replies with their ids

--- routes/api.js
+++ routes/api.js
@@ -12,6 +12,18 @@ const messageBoard = new mongoose.Schema({
 
 const MessageBoard = new mongoose.model("MessageBoard", messageBoard);
 
+const repliesToThreadText = new mongoose.Schema({
+  threadId: String,
+  threadText: String,
+  repliesToThreadText: String,
+  passwordToDelete: String,
+});
+
+const RepliesToThreadText = new mongoose.model(
+  "RepliesToThreadText",
+  repliesToThreadText
+);
+
 module.exports = function (app) {
   app.route("/api/threads/:board").post(function (req, res) {
     // console.log("req.body", req.body);
@@ -19,42 +31,115 @@ module.exports = function (app) {
     // console.log(board, boardText, passwordToDelete);
 
     async function createMessageBoard(board, boardText, passwordToDelete) {
-      const existingMessageBoard = await MessageBoard.findOne({ board: board });
-      console.log("existingMessageBoard. 1. :", existingMessageBoard);
-      if (existingMessageBoard) {
-        // console.log("existingMessageBoard. 2. :", existingMessageBoard);
-        return {
-          board: existingMessageBoard.board,
-          boardText: existingMessageBoard.boardText,
-          passwordToDelete: existingMessageBoard.passwordToDelete,
-        };
-      } else {
-        const newMessageBoard = await MessageBoard.create({
-          board: board,
-          boardText: boardText,
-          passwordToDelete: passwordToDelete,
-        });
+      // const existingMessageBoard = await MessageBoard.findOne({
+      //   board: board,
+      // });
+      // // console.log("existingMessageBoard. 1. :", existingMessageBoard);
+      // if (existingMessageBoard) {
+      //   // console.log("existingMessageBoard. 2. :", existingMessageBoard);
+      //   return {
+      //     board: existingMessageBoard.board,
+      //     boardText: existingMessageBoard.boardText,
+      //     passwordToDelete: existingMessageBoard.passwordToDelete,
+      //   };
+      // } else {
+      const newMessageBoard = await MessageBoard.create({
+        board: board,
+        boardText: boardText,
+        passwordToDelete: passwordToDelete,
+      });
 
-        await newMessageBoard.save();
-        console.log("newMessageBoard", newMessageBoard);
-        return {
-          board: newMessageBoard.board,
-          boardText: newMessageBoard.boardText,
-          passwordToDelete: newMessageBoard.passwordToDelete,
-        };
-      }
+      await newMessageBoard.save();
+      // console.log("newMessageBoard", newMessageBoard);
+
+      const messages = await MessageBoard.find({});
+
+      return messages;
+      // return {
+      //   board: newMessageBoard.board,
+      //   boardText: newMessageBoard.boardText,
+      //   passwordToDelete: newMessageBoard.passwordToDelete,
+      // };
+      // }
     }
 
     let message = createMessageBoard(board, boardText, passwordToDelete);
     message.then((data) => {
-      // console.log("data", data);
-      res.json({
-        board: data.board,
-        boardText: data.boardText,
-        passwordToDelete: data.passwordToDelete,
+      console.log("data", data);
+      res.send(data);
+      // res.json({
+      //   board: data.board,
+      //   boardText: data.boardText,
+      //   passwordToDelete: data.passwordToDelete,
+      // });
+    });
+  });
+  // .get(function (req, res) {
+  //   console.log(req.body);
+  // });
+
+  app.route("/api/replies/:board").post(function (req, res) {
+    const { threadId, threadText, repliesToThreadText, passwordToDelete } =
+      req.body;
+
+    console.log(threadId, threadText, repliesToThreadText, passwordToDelete);
+    // a function to asynchrnously create and save new reply
+    async function createAndSaveNewReply(
+      threadId,
+      threadText,
+      repliesToThreadText,
+      passwordToDelete
+    ) {
+      // create new reply
+      let newReply = await RepliesToThreadText.create({
+        threadId: threadId,
+        threadText: threadText,
+        repliesToThreadText: repliesToThreadText,
+        passwordToDelete: passwordToDelete,
+      });
+
+      // save new reply
+      newReply.save();
+    }
+
+    // create and save new reply
+    createAndSaveNewReply(
+      threadId,
+      threadText,
+      repliesToThreadText,
+      passwordToDelete
+    );
+
+    // a function to search the collection of replies for all replies matching thread id
+    async function searchForAllRepliesToThreadText(threadId) {
+      let allRepliesToThreadText = await RepliesToThreadText.find({
+        threadId: threadId,
+      });
+      return allRepliesToThreadText;
+    }
+
+    // search in the collection of replies for all replies matching thread id
+    let allRepliesToThreadText = searchForAllRepliesToThreadText(threadId);
+    allRepliesToThreadText.then((data) => {
+      // console.log("allRepliesToThreadText", data);
+
+      // iterate the list of replies and collect replies and their ids
+      let replies = [];
+      data.forEach((reply) => {
+        replies.push({
+          replyId: reply._id,
+          reply: reply.repliesToThreadText,
+        });
+      });
+
+      // console.log("replies", replies);
+
+      // return thread id, thread text, list of replies with their ids
+      res.send({
+        threadId: threadId,
+        threadText: threadText,
+        replies: replies,
       });
     });
   });
-
-  app.route("/api/replies/:board");
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -7,23 +7,51 @@ chai.use(chaiHttp);
 
 suite("Functional Tests", function () {
   suite("Integration tests with chai-http", function () {
-    test("1. Creating a new thread: POST request to /api/threads/{board}", (done) => {
+    // test("1. Creating a new thread: POST request to /api/threads/{board}", (done) => {
+    //   chai
+    //     .request(server)
+    //     .post("/api/threads/{board}")
+    //     .send({
+    //       board: "test",
+    //       boardText: "Test Test",
+    //       passwordToDelete: "123",
+    //     })
+    //     .end(function (req, res) {
+    //       // console.log("Test 1 response:", res.body);
+    //       assert.equal(res.status, "200");
+    //       // assert.property(res.body, "board");
+    //       // assert.property(res.body, "boardText");
+    //       // assert.property(res.body, "passwordToDelete");
+    //       done();
+    //     });
+    // });
+
+    test("A. Create replies to threads: POST request to /api/replies/{board}", (done) => {
       chai
         .request(server)
-        .post("/api/threads/{board}")
+        .post("/api/replies/{board}")
         .send({
-          board: "test",
-          boardText: "Test Test",
+          threadId: "634a4de84e5b9541698ac7f2",
+          threadText: "Test Test",
+          repliesToThreadText: "Thats good!",
           passwordToDelete: "123",
         })
-        .end(function (req, res) {
-          console.log("Test 1 response:", res.body);
+        .end(function (err, res) {
+          console.log("Test A response: ", res.body);
           assert.equal(res.status, "200");
-          assert.property(res.body, "board");
-          assert.property(res.body, "boardText");
-          assert.property(res.body, "passwordToDelete");
           done();
         });
     });
+
+    // test("2. Viewing the 10 most recent threads with 3 replies each: GET request to /api/threads/{board}", (done) => {
+    //   chai
+    //     .request(server)
+    //     .get("/api/threads/{board}")
+    //     .send({ recentThreads: 10, replies: 3 })
+    //     .end(function (res, err) {
+    //       console.log("Test 2 response:", res.body);
+    //       done();
+    //     });
+    // });
   });
 });
